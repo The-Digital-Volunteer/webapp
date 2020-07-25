1bf2e8976bc952303bb56ffd3ae43eb1190b91d4
kaisian <kaisian97@hotmail.com>
2020-07-25 20:58:17 +0800

    Add google map for hero

--- src/components/pages/helper/map.js
+++ src/components/pages/helper/map.js
@@ -1,25 +1,82 @@
 import React from 'react'
-
-import MapContainer from './googlemap'
-
+import MapContainer from '../shared/googlemap'
+import Header from './header'
+import { Link } from 'react-router-dom'
+import Profile from '../../../assets/img/hero-header/profile.svg'
+import Chat from '../../../assets/img/hero-header/chat.svg'
+import RateLooper from '../../../assets/img/hero-header/rateLooper.svg'
+import HallofFame from '../../../assets/img/hero-header/hallOfFame.svg'
+import HeroLog from '../../../assets/img/hero-header/heroLog.svg'
+import Task from '../../../assets/img/hero-header/task.svg'
+import Map from '../../../assets/img/hero-header/map.svg'
 class RequestMap extends React.Component {
   state = {
-    apiKey: process.env.REACT_APP_API_KEY,
+    apiKey: '',
   }
+
+  menus = [
+    {
+      img: Profile,
+      to: '/profile',
+      text: 'Profile',
+    },
+    {
+      img: Chat,
+      to: '/chat',
+      text: 'Chat',
+    },
+    {
+      img: RateLooper,
+      to: '/rate-looper',
+      text: 'Rate a Looper',
+    },
+    {
+      img: HallofFame,
+      to: '/hall-of-fame',
+      text: 'Hall of Fame',
+    },
+    {
+      img: HeroLog,
+      to: '/hero-log',
+      text: 'My Hero Log',
+    },
+    {
+      img: Task,
+      to: '/tasks',
+      text: 'My Tasks',
+    },
+    {
+      img: Map,
+      to: '/map',
+      text: 'The Map',
+    },
+    {
+      to: '/request',
+      text: 'I need a Hero',
+    },
+  ]
   render() {
     return (
-      <div className='wrapper'>
-        <section id='map-wrapper'>
-          <div className='text-box'>
-            <h3>Here you can see all people who need help</h3>
-            <p>
-              Here you can see where people who need help are located. Click on
-              the pins to see information about the volunteers.
-            </p>
-          </div>
-          <MapContainer apiKey={this.state.apiKey} />
-        </section>
-      </div>
+      <>
+        <Header menus={this.menus} />
+        <div className='wrapper'>
+          <section id='map-wrapper'>
+            <div className='text-box'>
+              <p className='title'>Who need help?</p>
+              <p className='subtitle'>
+                Click on the pins to see the task details
+                <Link
+                  className='subtitle-right'
+                  to='/request/map'
+                >
+                  I need help
+                </Link>
+              </p>
+            </div>
+            <MapContainer apiKey={this.state.apiKey} />
+          </section>
+        </div>
+      </>
     )
   }
 }
--- src/components/pages/request/map.js
+++ src/components/pages/request/map.js
@@ -1,27 +1,71 @@
-import React from "react";
+import React from 'react'
+import Header from '../helper/header'
+
+import Profile from '../../../assets/img/hero-header/looper.svg'
+import Chat from '../../../assets/img/hero-header/chat.svg'
+import RateLooper from '../../../assets/img/hero-header/rateLooper.svg'
+import HallofFame from '../../../assets/img/hero-header/hallOfFame.svg'
+import HeroLog from '../../../assets/img/hero-header/heroLog.svg'
+import Task from '../../../assets/img/hero-header/task.svg'
 
 class RequestMap extends React.Component {
+  menus = [
+    {
+      img: Profile,
+      to: '/profile',
+      text: 'Profile',
+    },
+    {
+      img: Chat,
+      to: '/chat',
+      text: 'Chat',
+    },
+    {
+      img: RateLooper,
+      to: '/rate-looper',
+      text: 'Rate a Hero',
+    },
+    {
+      img: HallofFame,
+      to: '/hall-of-fame',
+      text: 'Hall of Fame',
+    },
+    {
+      img: HeroLog,
+      to: '/hero-log',
+      text: 'My Hero Log',
+    },
+    {
+      img: Task,
+      to: '/tasks',
+      text: 'My Requests',
+    },
+    {
+      to: '/help',
+      text: 'Offer help',
+    },
+  ]
   render() {
     return (
-      <div className="wrapper">
-        <section id="registration_finish">
-          <div className="text-box">
-            <h3>
-              Here you can see all people who can help
-            </h3>
-            <p>
-              Here you can see where people who can help are located.
-              Click on the pins to see information about the volunteers.
-            </p>
-          </div>
+      <>
+        <Header menus={this.menus} />
+        <div className='wrapper'>
+          <section id='registration_finish'>
+            <div className='text-box'>
+              <h3>Here you can see all people who can help</h3>
+              <p>
+                Here you can see where people who can help are
+                located. Click on the pins to see information
+                about the volunteers.
+              </p>
+            </div>
 
-          <div>
-            Filter pins and map...
-          </div>
-        </section>
-      </div>
-    );
+            <div>Filter pins and map...</div>
+          </section>
+        </div>
+      </>
+    )
   }
 }
 
-export default RequestMap;
+export default RequestMap
