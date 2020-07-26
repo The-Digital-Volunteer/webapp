7dc1f267566f76d3eb0c00754e489de90924b3c2
Esiotrot <likkiravula@gmail.com>
2020-07-26 21:55:20 +0530

    Hero profile

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hero from '../src/components/pages/request/Myprofile/Heroprofile';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hero />
   </React.StrictMode>,
   document.getElementById('root')
 );
